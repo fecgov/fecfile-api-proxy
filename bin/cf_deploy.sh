85b76f83c5a7f5c9701fa6529bf1d9148dda2c51
David Heitzer <david.heitzer@tcg.com>
2025-01-29 17:17:45 -0500

    1744 update api proxy to deploy to multiple space

--- bin/cf_deploy.sh
+++ bin/cf_deploy.sh
@@ -11,54 +11,63 @@ org=${2}
 branch=${3}
 web_api="fecfile-web-api"
 
+cf_deploy_for_space() {
+    local space=$1
+    manifest=manifest_${space}.yml
+
+    # Get username/password for relevant space (uppercased)
+    cf_username_label="FEC_CF_USERNAME_$(echo $space | tr [a-z] [A-Z])"
+    cf_password_label="FEC_CF_PASSWORD_$(echo $space | tr [a-z] [A-Z])"
+
+    if [[ -z ${org} || -z ${branch} || -z ${app} ]]; then
+      echo "Usage: $0  <app> <org> <branch>" >&2
+      exit 1
+    fi
+
+    (
+      set +x # Disable debugging
+
+      if [[ -n ${!cf_username_label} && -n ${!cf_password_label} ]]; then
+        cf api ${cloud_gov}
+        cf auth "${!cf_username_label}" "${!cf_password_label}"
+      fi
+    )
+
+    # Target space
+    cf target -o ${org} -s ${space}
+
+    # If the app exists, use rolling deployment
+    if cf app ${app}; then
+      command="push --strategy rolling"
+    else
+      command="push"
+    fi
+
+    # Deploy app
+    cf ${command} ${app} -f ${manifest}
+
+    # Add network policies
+    cf add-network-policy ${app} ${web_api}
+}
+
 # Get the space that corresponds with the branch name
 if [[ ${branch} == "develop" ]]; then
     echo "Branch is develop, deploying to dev space"
-    space="dev"
+    cf_spaces="dev"
 elif [[ ${branch} == release* ]]; then
     echo "Branch starts with release, deploying to stage space"
-    space="stage"
+    cf_spaces="stage"
 elif [[ ${branch} == "main" ]]; then
     echo "Branch is main, deploying to test space"
-    space="test"
+    cf_spaces="test prod"
 else
 # Don't deploy other branches, pass build
     echo "No space detected"
     exit 0
 fi
 
-manifest=manifest_${space}.yml
-
-# Get username/password for relevant space (uppercased)
-cf_username_label="FEC_CF_USERNAME_$(echo $space | tr [a-z] [A-Z])"
-cf_password_label="FEC_CF_PASSWORD_$(echo $space | tr [a-z] [A-Z])"
-
-if [[ -z ${org} || -z ${branch} || -z ${app} ]]; then
-  echo "Usage: $0  <app> <org> <branch>" >&2
-  exit 1
+if [ ! -z "$cf_spaces" ];
+  for cf_space in $cf_spaces; do
+      cf_deploy_for_space $cf_space
+  done
 fi
-
-(
-  set +x # Disable debugging
-
-  if [[ -n ${!cf_username_label} && -n ${!cf_password_label} ]]; then
-    cf api ${cloud_gov}
-    cf auth "${!cf_username_label}" "${!cf_password_label}"
-  fi
-)
-
-# Target space
-cf target -o ${org} -s ${space}
-
-# If the app exists, use rolling deployment
-if cf app ${app}; then
-  command="push --strategy rolling"
-else
-  command="push"
-fi
-
-# Deploy app
-cf ${command} ${app} -f ${manifest}
-
-# Add network policies
-cf add-network-policy ${app} ${web_api}
